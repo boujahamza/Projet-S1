66480a9acad02452ed83772a3fdc3dc004713fd0
boujahamza <73479328+boujahamza@users.noreply.github.com>
2021-02-21 17:39:19 +0100

    Delete isConnected.php

--- isConnected.php
+++
@@ -1,8 +0,0 @@
-<?php
-//CHECK IF ALREADY SIGNED IN
-if(!isset($_SESSION)) {session_start();}
-            
-if(!(isset($_SESSION["isConnected"]) and $_SESSION["isConnected"] == "true")){
-	$_SESSION["isConnected"] = "false";
-}
-?>		
